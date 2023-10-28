5b5955169029ec3e0b94a089273eb6ed6fa19254
Thomas <nyumat18@gmail.com>
2023-10-28 02:04:30 -0700

    🚂 Transitions Group Fixed

--- core/src/pages/onboarding/index.tsx
+++ core/src/pages/onboarding/index.tsx
@@ -2,19 +2,22 @@ import { Link } from 'react-router-dom';
 
 export function One() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-4xl">One</h1>
-      <Link to="/2">Two</Link>
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl">One</h1>
+        <Link to="/2">Two</Link>
+      </div>
+    </>
   );
 }
 
 export function Two() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-4xl">Two</h1>
-      <Link to="/1">One</Link>
-      <Link to="/">Home</Link>
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl">Two</h1>
+        <Link to="/1">One</Link>
+      </div>
+    </>
   );
 }
