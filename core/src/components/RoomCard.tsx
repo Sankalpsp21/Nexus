11983d19e6b7bf04aa43f08cf033cc07c7ead2de
derek-williams00 <derekOfficial00@gmail.com>
2023-10-28 14:26:08 -0700

    white space formatting for RoomCard

--- core/src/components/RoomCard.tsx
+++ core/src/components/RoomCard.tsx
@@ -11,42 +11,42 @@ roomCode: string;
 
 
 const RoomCard: React.FC<RoomCardProps> = ({ userName, roomName, roomCode }) => {
-const hmsActions = useHMSActions();
-const [inputValues] = useState({
-//Name will be from clerk
-//Room token is in props
-userName: "",
-roomCode: ""
-});
+  const hmsActions = useHMSActions();
+  const [inputValues] = useState({
+    //Name will be from clerk
+    //Room token is in props
+    userName: "",
+    roomCode: ""
+  });
 
 
-const handleSubmit = async (e: { preventDefault: () => void; }) => {
-e.preventDefault();
-const {
-userName = '',
-roomCode = '',
-} = inputValues
-// use room code to fetch auth token
-const authToken = await hmsActions.getAuthTokenByRoomCode({ roomCode })
-try {
-await hmsActions.join({ userName, authToken });
-} catch (e) {
-console.error(e)
-}
-};
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    const {
+      userName = '',
+      roomCode = '',
+    } = inputValues
+    // use room code to fetch auth token
+    const authToken = await hmsActions.getAuthTokenByRoomCode({ roomCode })
+    try {
+      await hmsActions.join({ userName, authToken });
+    } catch (e) {
+      console.error(e)
+    }
+  };
 
-// Handle leaving room
-useEffect(() => {
-    window.onunload = () => {
-      hmsActions.leave();
-    };
-  }, [hmsActions]);
+  // Handle leaving room
+  useEffect(() => {
+      window.onunload = () => {
+        hmsActions.leave();
+      };
+    }, [hmsActions]);
 
-return (
-<div onClick={handleSubmit}>
-<h2>Join the {roomName} room!</h2>
-</div>
-);
+  return (
+    <div onClick={handleSubmit}>
+      <h2>Join the {roomName} room!</h2>
+    </div>
+  );
 };
 
 
