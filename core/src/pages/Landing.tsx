f6f7bbf6fa5992c68bb0d2e27be60b80c1aad2b7
Thomas <nyumat18@gmail.com>
2023-10-28 02:37:34 -0700

    🚀 Landing Page Improv

--- core/src/pages/Landing.tsx
+++ core/src/pages/Landing.tsx
@@ -1,14 +1,26 @@
-import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut } from '@clerk/clerk-react';
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, useAuth, useClerk } from '@clerk/clerk-react';
 import HeaderText from '../components/HeaderText';
 
 export default function Landing() {
+  const { isSignedIn } = useAuth();
+  const clerk = useClerk();
+
+  const handleClick = () => {
+    if (!isSignedIn) {
+      clerk.openSignUp();
+    }
+  };
+
   return (
     <>
       <ClerkLoaded>
         <SignedOut>
           <div className="flex flex-col items-center justify-center">
             <HeaderText />
-            <button className="btn btn-wide bg-950 hover:bg-300 text-50 hover:text-950 outline outline-1 outline-300 hover:outline-950">
+            <button
+              className="btn btn-wide bg-950 hover:bg-300 text-50 hover:text-950 outline outline-1 outline-300 hover:outline-950 scale-125"
+              onClick={handleClick}
+            >
               <span className="text-50 text-xl whitespace-nowrap">Join Nexus Today</span>
             </button>
           </div>
