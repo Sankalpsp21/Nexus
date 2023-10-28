bf3d125ce512035300d5c40f529596292912a43a
solderq35 <solderq35@gmail.com>
2023-10-28 02:13:08 -0700

    progress bar poc

--- core/src/components/NavBar.tsx
+++ core/src/components/NavBar.tsx
@@ -7,15 +7,11 @@ const NavBar: React.FC = () => {
     <nav className="nav p-4 max-h-15 bg-slate-200">
       <ul className="flex flex-row justify-between">
         <li className="mr-4">
-            <a href="/">
-                <figure className="w-36">
-                    <img
-                        src="/nexus-logo.svg"
-                        alt="Nexus logo"
-                        className="rounded-xl"
-                    />
-                </figure>
-            </a>
+          <a href="/">
+            <figure className="w-36">
+              <img src="/nexus-logo.svg" alt="Nexus logo" className="rounded-xl" />
+            </figure>
+          </a>
         </li>
         {/* The logo */}
         <SignedIn>
--- core/src/pages/onboarding/index.tsx
+++ core/src/pages/onboarding/index.tsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
+import ProgressBar from '@ramonak/react-progress-bar';
 
 export function One() {
   return (
     <>
+      <ProgressBar completed={30} isLabelVisible={false} />
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-4xl">One</h1>
         <Link to="/2">Two</Link>
@@ -14,6 +16,7 @@ export function One() {
 export function Two() {
   return (
     <>
+      <ProgressBar completed={60} isLabelVisible={false} />
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-4xl">Two</h1>
         <Link to="/1">One</Link>
