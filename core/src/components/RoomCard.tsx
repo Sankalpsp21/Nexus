b3d91f6fbb8548328ede50a7171aa470b4ce9110
Sankalp Patil <patilsa@oregonstate.edu>
2023-10-28 13:55:42 -0700

    RoomCard component

---
+++ core/src/components/RoomCard.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useState } from "react";
+import { useHMSActions } from "@100mslive/react-sdk";
+
+
+interface RoomCardProps {
+userName: string;
+roomName: string;
+roomCode: string;
+}
+
+
+const RoomCard: React.FC<RoomCardProps> = ({ userName, roomName, roomCode }) => {
+const hmsActions = useHMSActions();
+const [inputValues] = useState({
+//Name will be from clerk
+//Room token is in props
+userName: "",
+roomCode: ""
+});
+
+
+const handleSubmit = async (e: { preventDefault: () => void; }) => {
+e.preventDefault();
+const {
+userName = '',
+roomCode = '',
+} = inputValues
+// use room code to fetch auth token
+const authToken = await hmsActions.getAuthTokenByRoomCode({ roomCode })
+try {
+await hmsActions.join({ userName, authToken });
+} catch (e) {
+console.error(e)
+}
+};
+
+
+return (
+<div onClick={handleSubmit}>
+<h2>Join the {roomName} room!</h2>
+</div>
+);
+};
+
+
+export default RoomCard;
+
+
+
