798646b19009faafd7bc57170cd422e88ec7adf0
Thomas <nyumat18@gmail.com>
2023-10-28 12:56:45 -0700

    ⚡️ More Styling Polish

--- core/src/components/IconButton.tsx
+++ core/src/components/IconButton.tsx
@@ -5,7 +5,7 @@ interface IconButtonProps {
 
 export default function IconButton({ name, icon }: IconButtonProps) {
   return (
-    <div className="flex flex-col justify-center items-center flex-wrap border-2 shadow-sm p-2 hover:bg-100 hover:shadow-2xl transition duration-300 ease-in-out cursor-pointer rounded-xl">
+    <div className="flex flex-col justify-center bg-white items-center flex-wrap border-2 shadow-sm p-2 hover:bg-100 hover:shadow-2xl transition duration-300 ease-in-out cursor-pointer rounded-xl">
       <div className="flex flex-row items-center justify-center my-4 scale-150">{icon}</div>
       <h2 className="text-lg text-center">{name}</h2>
     </div>
--- core/src/pages/onboarding/index.tsx
+++ core/src/pages/onboarding/index.tsx
@@ -1,13 +1,15 @@
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
+import { MdNavigateNext } from 'react-icons/md';
 import IconButton from '../../components/IconButton';
 import { ICONS } from '../../components/icons'; // import ICONS
-import { MdNavigateNext } from 'react-icons/md';
 
 export function JoinGroups() {
   const navigate = useNavigate();
   return (
     <>
+      <h1 className="text-4xl mx-16 mt-4">Tell us what you're interests are</h1>
+      <p className="text-xl mx-16 mt-4 italic font-light">This will help us match you with the right groups.</p>
       <div className="flex flex-col items-center justify-center">
         <div className="grid grid-cols-8 gap-8 mx-12 my-12">
           {ICONS.map((icon) => (
@@ -16,7 +18,7 @@ export function JoinGroups() {
             </>
           ))}
         </div>
-        <button className="btn bg-950 hover:bg-300" onClick={() => navigate('/2')}>
+        <button className="btn bg-950 hover:bg-300 mb-4" onClick={() => navigate('/2')}>
           <MdNavigateNext className="text-white h-10 w-10" />
         </button>
       </div>
@@ -28,15 +30,19 @@ export function Two() {
   const navigate = useNavigate();
   return (
     <>
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-4xl">Two</h1>
-        <textarea
-          className="textarea textarea-bordered w-11/12 h-96 mb-4"
-          placeholder="Tell us about your interests"
-        ></textarea>
-        <button className="btn bg-950 hover:bg-300" onClick={() => navigate('/1')}>
-          <MdNavigateNext className="text-white h-10 w-10" />
-        </button>
+      <div className="flex flex-col items-center justify-start space-y-4">
+        <h1 className="text-4xl my-2">Let us learn a little bit more about you.</h1>
+        <p className="text-xl italic font-light">Your response help us match you with the right groups.</p>
+        <div className="flex flex-col items-center justify-center space-y-4 p-8">
+          <textarea
+            className="textarea textarea-bordered w-[150%] h-96 mb-4 bg-white opacity-75"
+            placeholder="Who are you? What do you do for fun? Let's get to know each other..."
+          ></textarea>
+          <button className="btn bg-950 hover:bg-300" onClick={() => navigate('/1')}>
+            <span className="text-50">Done</span>
+            <MdNavigateNext className="text-white h-10 w-10" />
+          </button>
+        </div>
       </div>
     </>
   );
