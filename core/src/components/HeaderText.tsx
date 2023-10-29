be8cb768fb0268ef5c94f2d5667f89c46b6923eb
Thomas <nyumat18@gmail.com>
2023-10-29 10:46:29 -0700

    HeaderText

--- core/src/components/HeaderText.tsx
+++ core/src/components/HeaderText.tsx
@@ -1,11 +1,19 @@
 export default function HeaderText() {
   return (
-    <div className="flex flex-row items-center justify-center h-[50vh]">
+    <div className="flex flex-col items-center justify-center h-[50vh]">
       <h1 className="text-8xl text-950 font-bold text-center">
         A better way to do
         <br />
         <span className="text-300">conversations</span>
       </h1>
+
+      <div className="flex flex-row items-center justify-center mt-16">
+        <h3 className="text-4xl text-950 font-bold text-center">
+          <span className="text-300">Nexus</span> is a new way to
+          <span className="text-300"> connect </span>
+          with those around you.
+        </h3>
+      </div>
     </div>
   );
 }
