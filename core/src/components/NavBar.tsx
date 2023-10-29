b1bca67342fc8ceb70075da590731ba4138d3e24
Sankalp Patil <patilsa@oregonstate.edu>
2023-10-28 19:07:41 -0700

    fixes

--- core/src/components/NavBar.tsx
+++ core/src/components/NavBar.tsx
@@ -7,9 +7,9 @@ const NavBar: React.FC = () => {
     <nav className="nav p-4 max-h-15 bg-slate-200">
       <ul className="flex flex-row justify-between">
         <li className="mr-4">
-          <a href="/">
+          <a href="/dashboard">
             <figure className="w-36">
-              <img src="/nexus-logo.svg" alt="Nexus logo" className="rounded-xl" />
+              <img src="/nexus-logo.svg" alt="Nexus logo" className="rounded-xl scale-150 -translate-y-7" />
             </figure>
           </a>
         </li>
