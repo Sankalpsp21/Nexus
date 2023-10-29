19cd970d75b0866c7bb4dd58c95531cf84e18b59
Sankalp Patil <patilsa@oregonstate.edu>
2023-10-29 08:52:13 -0700

    😳 enter the nexus

--- core/src/components/SideBar.tsx
+++ core/src/components/SideBar.tsx
@@ -4,8 +4,16 @@ export default function SideBar() {
   return (
     <div className="relative float-left w-64 bg-100 text-white p-4 opacity-100 overflow-hidden sm:opacity-100 sm:static sm:w-64 sm:block sm:top-0 sm:left-0 sm:bottom-0 sm:overflow-y-auto sm:overflow-x-hidden sm:flex-shrink-0 sm:transition-all sm:duration-300">
       <div className="h-full max-h-[84.3vh] overflow-y-auto no-scrollbar">
+        
+        <h2 className="text-lg font-bold text-800 mb-1">Match with a user</h2>
+        <div className='flex justify-center mb-3'>
+          <a href="/dashboard" className='btn btn-primary hover:bg-200'>
+              Enter the Nexus
+          </a>
+        </div>
+
         <h2 className="text-lg font-bold text-800">Active Nexus Users</h2>
-        <ul>
+        <ul className='mb-3'>
           {Array.from({ length: 5 }, (_, index) => index + 1).map((item) => (
             <li key={item} className="flex items-center space-x-4 space-y-2">
               <div className="avatar">
@@ -22,6 +30,7 @@ export default function SideBar() {
             </li>
           ))}
         </ul>
+
         <h2 className="text-xl font-bold text-800">Active Nexus Rooms</h2>
         <ul>
           {Array.from({ length: 7 }, (_, index) => index + 1).map((item) => (
