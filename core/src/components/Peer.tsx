b723c4fe2850b3e5835be2887e67997df0e78692
Tom Nyuma <46255836+Nyumat@users.noreply.github.com>
2023-10-29 08:06:24 -0700

    🕺🏾 Lobbies and Loaders

--- core/src/components/Peer.tsx
+++ core/src/components/Peer.tsx
@@ -1,8 +1,10 @@
-function Peer({ peer } : any) {
+import { useVideo } from '@100mslive/react-sdk';
+
+function Peer({ peer }: any) {
   return (
     <div className="peer-container mb-24">
       <div className="peer-name">
-        {peer.name} {peer.isLocal ? "(You)" : ""}
+        {peer.name} {peer.isLocal ? '(You)' : ''}
       </div>
     </div>
   );
