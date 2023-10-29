f565a464f5bf33d0104612ef93eb42b3729d2dfe
solderq35 <solderq35@gmail.com>
2023-10-29 08:56:01 -0700

    convec funcs

--- core/convex/users.ts
+++ core/convex/users.ts
@@ -15,3 +15,21 @@ export const makeUserInitialLoginFalse = mutation({
             await ctx.db.patch(id, { initialLogin: false });
       }
 });
+
+export const getUserPrefrences = mutation({
+      args: { userId: v.id("users"), prefrences: v.array(v.string()) },
+      handler: async (ctx, args) => {
+        const userObj = await ctx.db.query("users")
+        .filter((q) => q.gte(q.field("prefrences"), 18))
+        .collect();
+        // on the frontend, check length of userObj`
+      },
+});
+
+export const updatePrefrences = mutation({
+      args: { id: v.id("users"), prefrences: v.array(v.string()) },
+      handler: async (ctx, args) => {
+            const id = args.id;
+            await ctx.db.patch(id, { prefrences: args.prefrences});
+      }
+});
